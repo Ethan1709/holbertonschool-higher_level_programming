2a9a9e2ced22bf466cc1daded9ecaf34b817c59e
Ethan1709 <benyayerethan@gmail.com>
2023-01-16 09:37:36 -0800

    fixing file 3

--- python-if_else_loops_functions/3-print_alphabt.py
+++ python-if_else_loops_functions/3-print_alphabt.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python3
 for i in range(97, 123):
-    print(chr(i) + 0 * "{:02}".format(0), end="")
-    if i == 101  or i == 113:
+    if i == 101 or i == 113:
         continue
+    print(chr(i) + 0 * "{:02}".format(0), end="")
