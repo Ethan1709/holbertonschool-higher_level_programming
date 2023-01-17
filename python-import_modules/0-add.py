5f3c82a80e8a42cdce84178e4030ff5e164d91b8
Ethan1709 <benyayerethan@gmail.com>
2023-01-17 11:01:07 -0800

    test

--- python-import_modules/0-add.py
+++ python-import_modules/0-add.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 def add(a, b):
-    a = 1
-    b = 2
     if __name__ == "__main__":
         import add_0
+        a = 1
+        b = 2
         print("{} + {} = {}".format(a, b, add))
