4289b2ecfa564024327ef5395d80bc8a023e28fa
Ethan1709 <benyayerethan@gmail.com>
2023-01-17 13:45:55 -0800

    fixing name and main

--- python-import_modules/3-infinite_add.py
+++ python-import_modules/3-infinite_add.py
@@ -1,4 +1,5 @@
 #!/usr/bin/python3
-import sys
-summ = sum(int(i) for i in sys.argv[1:])
-print(summ)
+if __name__ == "__main__":
+    import sys
+    summ = sum(int(i) for i in sys.argv[1:])
+    print(summ)
