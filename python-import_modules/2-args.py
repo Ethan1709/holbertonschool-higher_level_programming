8ce1ffa71ad30d209474093e28a5ca9137b506fa
Ethan1709 <benyayerethan@gmail.com>
2023-01-20 05:41:20 -0800

    fixing style

--- python-import_modules/2-args.py
+++ python-import_modules/2-args.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
-if __name__ == "__main__":
-    import sys
+import sys
 
+if __name__ == "__main__":
     for i in range(0, len(sys.argv)):
         i += 1
 
--- python-import_modules/3-infinite_add.py
+++ python-import_modules/3-infinite_add.py
@@ -1,5 +1,6 @@
 #!/usr/bin/python3
+import sys
+
 if __name__ == "__main__":
-    import sys
     summ = sum(int(i) for i in sys.argv[1:])
     print(summ)
