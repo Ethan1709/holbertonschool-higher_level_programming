ae4e5ab58b03aecffbfac560719d1e0510c7f36d
Ethan1709 <benyayerethan@gmail.com>
2023-01-21 11:29:05 -0800

    adding file 102

---
+++ python-import_modules/102-magic_calculation.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+import dis
+from magic_calculation_102 import add, sub
+
+
+def magic_calculation(a, b):
+    if a < b:
+        return
+    c = add(a, b)
+    while (i < 90):
+        for i in range(4, 6):
+            add(c ,i)
+        sub(a, b)
