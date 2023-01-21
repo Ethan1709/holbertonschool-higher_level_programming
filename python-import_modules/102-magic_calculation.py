93aeb2e6a68e2c871c713b2b6587eab009834346
Ethan1709 <benyayerethan@gmail.com>
2023-01-21 12:30:33 -0800

    fixing file 102

--- python-import_modules/102-magic_calculation.py
+++ python-import_modules/102-magic_calculation.py
@@ -5,8 +5,7 @@ from magic_calculation_102 import add, sub
 
 def magic_calculation(a, b):
     if a < b:
-        return
-    c = add(a, b)
+        return c
     for i in range(4, 6):
         c = add(c, i)
     return sub(a, b)
