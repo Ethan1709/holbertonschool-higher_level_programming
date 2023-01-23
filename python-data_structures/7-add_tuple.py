e4268c11879bf08b74078a4d1e7122d34a4e3ed4
Ethan1709 <benyayerethan@gmail.com>
2023-01-23 11:45:28 -0800

    fixing file 7

--- python-data_structures/7-add_tuple.py
+++ python-data_structures/7-add_tuple.py
@@ -6,15 +6,15 @@ def add_tuple(tuple_a=(), tuple_b=()):
     list_b = list(tuple_b)
 
     i = 0
-    for i in list_a:
-        i += 1    
+    for i in range(len(list_a)):
+        i += 1
     if i > 2:
         list_a[2:] = []
     if i == 1:
         list_a += [0]
     if i == 0:
         list_a += [0, 0]
-    
+
     j = 0
     for j in range(0, len(list_b)):
         j += 1
@@ -24,7 +24,6 @@ def add_tuple(tuple_a=(), tuple_b=()):
         list_b += [0]
     if j == 0:
         list_b += [0, 0]
-
 
 
     z = zip(list_a, list_b)
