9e408135c25c41cf01ee0ed659ad7ed6bbbd4ebd
Ethan1709 <benyayerethan@gmail.com>
2023-01-23 12:29:16 -0800

    fixing none cond

--- python-data_structures/8-multiple_returns.py
+++ python-data_structures/8-multiple_returns.py
@@ -2,8 +2,9 @@
 
 
 def multiple_returns(sentence):
-    if not sentence:
-        return
+    i = 0
     for i in range(len(sentence)):
         i += 1
+    if i == 0:
+        return (0, None)
     return (i, sentence[0])
