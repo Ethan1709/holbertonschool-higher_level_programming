38a54238086290f88b5b5b73a96ffa24f3b613cb
Ethan1709 <benyayerethan@gmail.com>
2023-01-24 05:54:50 -0800

    adding first files

---
+++ python-more_data_structures.py/0-square_matrix_simple.py
@@ -0,0 +1,7 @@
+#!/usr/bin/python3
+
+def square_matrix_simple(matrix=[]):
+    new = []
+    for i in range(len(matrix)):
+        new.append(list(map((lambda x: x ** 2), matrix[i])))
+    return new
