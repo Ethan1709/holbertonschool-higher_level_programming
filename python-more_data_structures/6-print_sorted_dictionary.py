fc0dd93253d1baea1822675dd06ac9d1a75df9f4
Ethan1709 <benyayerethan@gmail.com>
2023-01-25 01:48:51 -0800

    cosmic

--- python-more_data_structures/6-print_sorted_dictionary.py
+++ python-more_data_structures/6-print_sorted_dictionary.py
@@ -2,5 +2,5 @@
 
 
 def print_sorted_dictionary(a_dictionary):
-    for key, value in sorted(a_dictionary.items(), key = lambda x: x[0]):
-        print("{} : {}".format(key, value))
+    for key, value in sorted(a_dictionary.items(), key=lambda x: x[0]):
+        print("{}: {}".format(key, value))
