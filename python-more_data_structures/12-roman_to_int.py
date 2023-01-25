a760c89f2aa75b9719a4f907afe86e0eaaa7ddbf
Ethan1709 <benyayerethan@gmail.com>
2023-01-25 06:28:45 -0800

    cosmic

--- python-more_data_structures/12-roman_to_int.py
+++ python-more_data_structures/12-roman_to_int.py
@@ -2,12 +2,13 @@
 
 
 def roman_to_int(roman_string):
-    if not roman_string:
+    if not roman_string or None:
         return 0
     k = 0
     r = {'I': 1, 'V': 5, 'X': 10, 'L': 50, 'C': 100, 'D': 500, 'M': 1000}
     for i in range(len(roman_string)):
-        if i != (len(roman_string) - 1) and r[roman_string[i]] < r[roman_string[i + 1]]:
+        if i != (len(roman_string) - 1) and r[roman_string[i]] \
+                < r[roman_string[i + 1]]:
             k -= r[roman_string[i]]
         else:
             k += r[roman_string[i]]
