22f41515454d8b25ef65703d9d1f7e935ec8f3dd
Ethan1709 <benyayerethan@gmail.com>
2023-01-29 10:11:46 -0800

    commit cosmic

---
+++ python-more_data_structures/100-weight_average.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+def weight_average(my_list=[]):
+    if not my_list:
+        return 0
+    arez = [(x * y) for x, y in my_list]
+    othmane = [(y) for x, y in my_list]
+
+    total_arez = 0
+    for i in range(len(arez)):
+        total_arez = total_arez + arez[i]
+
+    total_othmane = 0
+    for j in range(len(othmane)):
+        total_othmane = total_othmane + othmane[i]
+
+    return(total_arez / total_othmane)
