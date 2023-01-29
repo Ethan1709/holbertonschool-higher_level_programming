7f5424be65a9395bfe219a52ab8c3d46f8ca8a8e
Ethan1709 <benyayerethan@gmail.com>
2023-01-29 10:15:25 -0800

    fixing othmane

--- python-more_data_structures/100-weight_average.py
+++ python-more_data_structures/100-weight_average.py
@@ -4,13 +4,10 @@ def weight_average(my_list=[]):
         return 0
     arez = [(x * y) for x, y in my_list]
     othmane = [(y) for x, y in my_list]
-
     total_arez = 0
     for i in range(len(arez)):
         total_arez = total_arez + arez[i]
-
     total_othmane = 0
     for j in range(len(othmane)):
-        total_othmane = total_othmane + othmane[i]
-
+        total_othmane = total_othmane + othmane[j]
     return(total_arez / total_othmane)
