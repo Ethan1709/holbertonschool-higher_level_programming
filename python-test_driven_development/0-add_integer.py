1366807999c0ec794cd9aaca0ef0f9825bf55623
Ethan1709 <benyayerethan@gmail.com>
2023-01-30 11:31:19 -0800

    adding README and file 0

---
+++ python-test_driven_development/0-add_integer.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+def add_integer(a, b=98):
+    try:
+        if type(a) is float:
+            a = int(a)
+        if type(b) is float:
+            b = int(b)
+        return (a + b)
+    except:
+        if type(a) is not int:
+            raise TypeError('a must be an integer')
+        if type(b) is not int:
+            raise TypeError('b must be an integer')
