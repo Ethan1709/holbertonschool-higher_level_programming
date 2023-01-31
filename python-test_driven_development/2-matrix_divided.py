25676298ea8de00838190db988975fce0b019bfe
Ethan1709 <benyayerethan@gmail.com>
2023-01-31 10:05:08 -0800

    fixing some tests

--- python-test_driven_development/2-matrix_divided.py
+++ python-test_driven_development/2-matrix_divided.py
@@ -6,10 +6,10 @@ def matrix_divided(matrix, div):
         raise TypeError('Each row of the matrix must have the same size')
     if div == 0:
         raise ZeroDivisionError('division by zero')
-    if type(div) is not int or type(div) is not float:
+    if type(div) is not int and type(div) is not float:
         raise TypeError('div must be a number')
     for i in range(len(matrix)):
-        if type(matrix[i]) is not int or type(matrix[i]) is not float:
+        if type(matrix[i]) is not int and type(matrix[i]) is not float:
             raise TypeError('matrix must be a matrix (list of lists) of integers/floats')
         new.append(list(map(lambda x: x / div, matrix[i])))
     rounded = [[round(x, 2) for x in row] for row in new]
