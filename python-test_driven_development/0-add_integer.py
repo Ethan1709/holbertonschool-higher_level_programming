e6e7fd585a9b14daf379eb0ae21d51a37a0f867e
Ethan1709 <benyayerethan@gmail.com>
2023-01-31 04:05:59 -0800

    adding test file

--- python-test_driven_development/0-add_integer.py
+++ python-test_driven_development/0-add_integer.py
@@ -4,14 +4,12 @@
 
 def add_integer(a, b=98):
     """ add two integers """
-    try:
-        if type(a) is float:
-            a = int(a)
-        if type(b) is float:
-            b = int(b)
-        return (a + b)
-    except:
-        if type(a) is not int:
-            raise TypeError('a must be an integer')
-        if type(b) is not int:
-            raise TypeError('b must be an integer')
+    if type(a) is float:
+        a = int(a)
+    if type(b) is float:
+        b = int(b)
+    if type(a) is not int:
+        raise TypeError('a must be an integer')
+    if type(b) is not int:
+       raise TypeError('b must be an integer')
+    return (a + b)
