3da578dae784d001cf6e27d2ce76661472e0a3a2
Ethan1709 <benyayerethan@gmail.com>
2023-01-31 10:56:05 -0800

    fixing len

--- python-test_driven_development/2-matrix_divided.py
+++ python-test_driven_development/2-matrix_divided.py
@@ -2,15 +2,16 @@
 def matrix_divided(matrix, div):
     new = []
     rounded = []
-    if len(matrix[0]) != len(matrix[1]):
+    if len(matrix) == 0 or len(matrix[0]) == 0:
+        raise TypeError("matrix must be a matrix (list of lists) of \
+integers/floats")
+    if len(matrix[0]) != len(matrix[1]) and len(matrix[1]) != 0:
         raise TypeError('Each row of the matrix must have the same size')
     if div == 0:
         raise ZeroDivisionError('division by zero')
     if type(div) is not int and type(div) is not float:
         raise TypeError('div must be a number')
     for i in range(len(matrix)):
-        if type(matrix[i]) is not int and type(matrix[i]) is not float:
-            raise TypeError('matrix must be a matrix (list of lists) of integers/floats')
         new.append(list(map(lambda x: x / div, matrix[i])))
     rounded = [[round(x, 2) for x in row] for row in new]
     return rounded
