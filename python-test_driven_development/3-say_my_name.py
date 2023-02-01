45aac3f1cb636a79385ed58ad32da166f118ec40
Ethan1709 <benyayerethan@gmail.com>
2023-02-01 02:01:37 -0800

    fixing files 3

--- python-test_driven_development/3-say_my_name.py
+++ python-test_driven_development/3-say_my_name.py
@@ -4,6 +4,9 @@
 
 def say_my_name(first_name, last_name=""):
     """ print name """
+    if first_name == None and last_name == None:
+        raise TypeError("say_my_name() missing 2 required positional arguments:\
+                'first_name' and 'last_name'")
     if type(first_name) is not str:
         raise TypeError("first_name must be a string")
     if type(last_name) is not str:
