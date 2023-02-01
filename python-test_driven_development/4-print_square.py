63d957e413698c5c3868a1ea698fc8481a9881b4
Ethan1709 <benyayerethan@gmail.com>
2023-02-01 02:37:22 -0800

    fixing none cond for file 4

--- python-test_driven_development/4-print_square.py
+++ python-test_driven_development/4-print_square.py
@@ -4,6 +4,8 @@
 
 def print_square(size):
     """ print square """
+    if size is None:
+        raise TypeError("print_square() missing 1 required positional argument: 'size'")
     if type(size) is float and size < 0:
         raise TypeError("size must be an integer")
     if type(size) is not int:
