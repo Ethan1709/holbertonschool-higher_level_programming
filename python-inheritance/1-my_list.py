143f09bb46588645d54d1ed8b49b9b539e1bbf53
Ethan1709 <benyayerethan@gmail.com>
2023-02-06 07:12:05 -0800

    test

--- python-inheritance/1-my_list.py
+++ python-inheritance/1-my_list.py
@@ -1,5 +1,4 @@
 #!/usr/bin/python3
-<<<<<<< HEAD
 """ inherit """
 
 
@@ -7,15 +6,3 @@ class MyList(list):
     """ inherit """
     def print_sorted(self):
         print(MyList)
-=======
-""" My list """
-
-
-class MyList(list):
-    """ My list """
-    def __init__(self):
-        super().__init__(self)
-
-    def print_sorted(self):
-        print(sorted(self))
->>>>>>> 61bf2fcd95ddac16bc853b27c6d1b85afdbd8fdb
