40a3eb3bf7dcce898cdc8db05153395d46bcf36f
Ethan1709 <benyayerethan@gmail.com>
2023-02-08 03:53:39 -0800

    fixing append

--- python-input_output/2-append_write.py
+++ python-input_output/2-append_write.py
@@ -4,6 +4,6 @@
 
 def append_write(filename="", text=""):
     """ write """
-    with open(filename, 'w', encoding="utf-8") as f:
+    with open(filename, 'a', encoding="utf-8") as f:
         f = f.write(text)
         return f
