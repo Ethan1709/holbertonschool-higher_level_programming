797d6cccc9c2103eb52efae019e872db6b13b9e8
Ethan1709 <benyayerethan@gmail.com>
2023-02-08 01:19:51 -0800

    adding file 11

--- python-inheritance/11-square.py
+++ python-inheritance/11-square.py
@@ -1,12 +1,12 @@
 #!/usr/bin/python3
-""" Base Geometry """
+""" Square """
 
 
 Rectangle = __import__('9-rectangle').Rectangle
 
 
 class Square(Rectangle):
-    """ Base Geometry """
+    """ Square """
     def __init__(self, size):
         self.__size = size
         self.integer_validator("size", size)
