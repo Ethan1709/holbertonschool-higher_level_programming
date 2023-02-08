3ca05c319009b646c366eb3d0c5c12ef09ce087e
Ethan1709 <benyayerethan@gmail.com>
2023-02-08 03:24:23 -0800

    fixing file 0

--- python-input_output/0-read_file.py
+++ python-input_output/0-read_file.py
@@ -6,6 +6,4 @@ def read_file(filename=""):
     """ read """
     with open(filename, encoding="utf-8") as f:
         if filename != "":
-            print(f.read()[:-1])
-        else:
-            print(end="")
+            print(f.read()[:-1], end="")
