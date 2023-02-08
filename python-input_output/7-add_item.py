4fd63c8fa02c4dd4b2260f30a968b9e6d3952b59
Ethan1709 <benyayerethan@gmail.com>
2023-02-08 08:09:07 -0800

    fixing file 7

--- python-input_output/7-add_item.py
+++ python-input_output/7-add_item.py
@@ -6,10 +6,13 @@ save_to_json_file = __import__('5-save_to_json_file').save_to_json_file
 load_from_json_file = __import__('6-load_from_json_file').load_from_json_file
 
 
-args = sys.argv
-filename = "add_item.json"
-with open(filename, 'a+', encoding="utf-8") as f:
-    my_list = []
-    my_list.extend(args[:])
-    save_to_json_file(my_list, filename)
-    load_from_json_file(filename)
+if __name__ == '__main__':
+    args = sys.argv
+    filename = "add_item.json"
+    with open(filename, 'a+', encoding="utf-8") as f:
+        try:
+            my_list = load_from_json_file(filename)
+        except:
+            my_list = []
+        my_list.extend(args[1:])
+        save_to_json_file(my_list, filename)
