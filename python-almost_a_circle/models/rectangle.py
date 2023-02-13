0a0e1baf15a06400bd1df25f1e1fd5747650c10f
Ethan1709 <benyayerethan@gmail.com>
2023-02-13 05:14:12 -0800

    updating task 5

--- python-almost_a_circle/models/rectangle.py
+++ python-almost_a_circle/models/rectangle.py
@@ -65,6 +65,7 @@ class Rectangle(Base):
         return self.__width * self.__height
 
     def display(self):
+        """ display the rectangle """
         for i in range(self.__height):
             for j in range(self.__width):
                 print("#", end="")
