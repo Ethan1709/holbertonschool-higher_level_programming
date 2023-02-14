a313c68c178e1f4dc91da2d838cf97ec87253051
Ethan1709 <benyayerethan@gmail.com>
2023-02-14 04:52:03 -0800

    update task 15

--- python-almost_a_circle/models/base.py
+++ python-almost_a_circle/models/base.py
@@ -13,8 +13,8 @@ class Base:
         else:
             Base.__nb_objects += 1
             self.id = Base.__nb_objects
-
-    
+ 
+    @staticmethod
     def to_json_string(list_dictionaries):
         """ dict to json string """
         if list_dictionaries is None or list_dictionaries == "":
