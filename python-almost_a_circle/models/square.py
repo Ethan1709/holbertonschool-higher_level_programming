b4d70b15e62fceb5bf02fff6116c5ca881f09571
Ethan1709 <benyayerethan@gmail.com>
2023-02-14 02:10:48 -0800

    update square task 11

--- python-almost_a_circle/models/square.py
+++ python-almost_a_circle/models/square.py
@@ -6,9 +6,17 @@ from models.rectangle import Rectangle
 class Square(Rectangle):
     """ square """
     def __init__(self, size, x=0, y=0, id=None):
-        self.size = size
+        self.__size = size
         super().__init__(size, size, x, y, id)
 
     def __str__(self):
         """ square """
         return f"[Square] ({self.id}) {self.x}/{self.y} - {self.width}"
+
+    @property
+    def size(self):
+        return self.__size
+
+    @size.setter
+    def size(self, value):
+        super(Square, self.__class__).width.fset(self, value)
