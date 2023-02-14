b8342aef7251aac3667b219fdbea996ed197e066
Ethan1709 <benyayerethan@gmail.com>
2023-02-14 05:55:41 -0800

    update task 17

--- python-almost_a_circle/models/base.py
+++ python-almost_a_circle/models/base.py
@@ -30,3 +30,9 @@ class Base:
 
         with open("%s.json" % (cls.__name__), 'w') as f:
             f.write(cls.to_json_string(l))
+    
+    @staticmethod
+    def from_json_string(json_string):
+        if json_string is None:
+            return json.loads([])
+        return json.loads(json_string)
