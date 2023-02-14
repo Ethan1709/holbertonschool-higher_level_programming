4581812098167fea2bf84763ea16ce18fe546ef8
Ethan1709 <benyayerethan@gmail.com>
2023-02-14 09:25:20 -0800

    ex19 meow

--- python-almost_a_circle/models/base.py
+++ python-almost_a_circle/models/base.py
@@ -49,3 +49,15 @@ class Base:
         r.update(**dictionary)
 
         return r
+
+    @classmethod
+    def load_from_file(cls):
+        """ meow """
+        try:
+            with open(cls.__name__ + '.json', 'r') as file:
+                text = file.read()
+        except:
+            text = None
+        
+        data = cls.from_json_string(text)
+        return [cls.create(**v) for v in data]
