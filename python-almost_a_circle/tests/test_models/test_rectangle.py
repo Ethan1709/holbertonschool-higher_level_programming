f47fa567431bfa5beb3ee03de32dac72e17670fb
Ethan1709 <benyayerethan@gmail.com>
2023-02-15 08:07:44 -0800

    update test rect

--- python-almost_a_circle/tests/test_models/test_rectangle.py
+++ python-almost_a_circle/tests/test_models/test_rectangle.py
@@ -52,12 +52,12 @@ class Test_Rectangle(unittest.TestCase):
             result = buffer.getvalue()
         self.assertEqual(result, expected_output)
 
-        #r1 = Rectangle()
-        #expected_output = ''
-        #with StringIO() as buffer, redirect_stdout(buffer):
-         #   r1.display()
-         #  result = buffer.getvalue()
-        #self.assertEqual(result, expected_output)
+        r1 = Rectangle(3, 2, 1, 1)
+        expected_output = '\n ###\n ###\n'
+        with StringIO() as buffer, redirect_stdout(buffer):
+           r1.display()
+           result = buffer.getvalue()
+        self.assertEqual(result, expected_output)
 
         r = Rectangle(1, 2, 3, 4)
         r.update(89)
