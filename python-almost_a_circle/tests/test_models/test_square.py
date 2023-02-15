4bbc064b39ccea6e80a5149f046aa82c45b3fbc4
Ethan1709 <benyayerethan@gmail.com>
2023-02-15 08:47:18 -0800

    adding tests square

--- python-almost_a_circle/tests/test_models/test_square.py
+++ python-almost_a_circle/tests/test_models/test_square.py
@@ -5,6 +5,7 @@ from models.square import Square
 from models.base import Base
 
 from io import StringIO
+from contextlib import redirect_stdout
 
 
 class Test_Square(unittest.TestCase):
@@ -12,10 +13,13 @@ class Test_Square(unittest.TestCase):
     def test_init(self):
         r = Square(8, 4, 2, 10)
         self.assertEqual(r.id, 10)
-        self.assertEqual(r.size, 8)
+        self.assertEqual(r.width, 8)
         self.assertEqual(r.x, 4)
         self.assertEqual(r.y, 2)
         self.assertEqual(r.area(), 64)
+
+        r  = Square(8)
+        self.assertEqual(r.width, 8)
 
         self.assertRaisesRegex(
         TypeError, "width must be an integer", Square, "8", 4, 2, 10)
@@ -30,22 +34,32 @@ class Test_Square(unittest.TestCase):
         self.assertRaisesRegex(
         ValueError, "y must be >= 0", Square, 8, 1, -2, 10)
         self.assertRaisesRegex(TypeError, "", Square, )
+        self.assertRaisesRegex(
+        ValueError, "width must be > 0", Square, 0, 4, 2, 10)
 
-        def test_display(self):
-            r1 = Square(3, 2)
-            expected_output = '###\n###\n'
-            with StringIO() as buffer, redirect_stdout(buffer):
-                r1.display()
-                result = buffer.getvalue()
-            self.assertEqual(result, expected_output)
+        r1 = Square(1)
+        expected_output = '#\n'
+        with StringIO() as buffer, redirect_stdout(buffer):
+            r1.display()
+            result = buffer.getvalue()
+        self.assertEqual(result, expected_output)
 
-        def test_update(self):
-            r = Square(1, 3, 4, 5)
-            self.assertEqual(r.update().id, 5)
-            self.assertEqual(r.update().width, 1)
-            self.assertEqual(r.update().x, 3)
-            self.assertEqual(r.update().y, 4)
+        r1 = Square(1, 1, 1)
+        expected_output = '\n #\n'
+        with StringIO() as buffer, redirect_stdout(buffer):
+           r1.display()
+           result = buffer.getvalue()
+        self.assertEqual(result, expected_output)
 
-        def test_str(self):
-            r = Square(14, 12, 15, 25)
-            self.assertEqual(str(r), "[Rectangle] (25) 12/15 - 14")
+        r = Square(1, 3, 4)
+        r.update(89)
+        self.assertEqual(r.id, 89)
+        r.update(89, 3)
+        self.assertEqual(r.width, 3)
+
+        r = Square(14, 12, 15, 25)
+        self.assertEqual(str(r), "[Square] (25) 12/15 - 14")
+
+        r = Square(2, 6, 8, 22)
+        self.assertEqual(r.to_dictionary(), {'id': 22, 'size': 2,
+                                             'x': 6, 'y': 8})
