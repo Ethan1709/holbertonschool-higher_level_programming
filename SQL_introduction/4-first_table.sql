ae657959c15171234458bbb0e212ab45d6b57b06
Ethan Benyayer <benyayerethan@gmail.com>
2023-03-13 12:05:17 +0100

    adding task 4

--- SQL_introduction/4-first_table.sql
+++ SQL_introduction/4-first_table.sql
@@ -1,5 +1,5 @@
 -- Write a script that creates a table called first_table in the current database
-CREATE TABLE IF EXISTS first_coffee (
+CREATE TABLE IF EXISTS first_table (
     -> id INT,
     -> name VARCHAR
 -> );
