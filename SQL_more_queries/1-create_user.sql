9147cbaa6cafdfd450d4022a5f7a1d749c41be67
Ethan1709 <benyayerethan@gmail.com>
2023-03-13 18:54:36 +0100

    adding task 1

--- SQL_more_queries/1-create_user.sql
+++ SQL_more_queries/1-create_user.sql
@@ -1,3 +1,3 @@
 -- Write a script that creates the MySQL server user user_0d_1.
 CREATE USER IF NOT EXISTS 'user_0d_1' IDENTIFIED BY 'user_0d_1_pwd';
-GRANTS ALL TO 'user_0d_1';
+GRANTS ALL PRIVILEGE ON *.* TO 'user_0d_1';
