6750430d798ffd39bf3937e0133646a9f3992621
Ethan1709 <benyayerethan@gmail.com>
2023-03-13 20:20:32 +0100

    adding task 6

--- SQL_more_queries/6-states.sql
+++ SQL_more_queries/6-states.sql
@@ -3,6 +3,6 @@ CREATE DATABASE IF NOT EXISTS hbtn_0d_usa;
 USE hbtn_0d_usa;
 CREATE TABLE IF NOT EXISTS states (
     id int UNIQUE AUTO-INCREMENT NOT NULL, 
-    PRIMARY KEY (id),
-    name VARCHAR(256) NOT NULL
+    name VARCHAR(256) NOT NULL,
+    PRIMARY KEY (id)
 );
