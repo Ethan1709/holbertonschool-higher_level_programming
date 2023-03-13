86ebef642d08ef31c5c7d84a1e1864e40270df81
Ethan Benyayer <benyayerethan@gmail.com>
2023-03-13 21:59:22 +0100

    adding task 9

--- SQL_introduction/9-full_creation.sql
+++ SQL_introduction/9-full_creation.sql
@@ -1,11 +1,9 @@
--- Write a script that creates a table second_table in the database hbtn_0c_0 and add multiples rows.
-CREATE TABLE IF NOT EXISTS second_table (
-    id int,
-    name VARCHAR(256),
-    score int;
-)
-
-INSERT INTO second_table VALUES (1, "John", 10);
-INSERT INTO second_table VALUES (2, "Alex", 3);
-INSERT INTO second_table VALUES (3, "Bob", 14);
-INSERT INTO second_table VALUES (4, "George", 8);
+CREATE TABLE second_table(id int, name VARCHAR(256), score int);
+INSERT INTO second_table (id, name, score)  
+SELECT 1, "John", 10 
+UNION ALL   
+SELECT 2, "Alex", 3  
+UNION ALL  
+SELECT 3, "Bob", 14
+UNION ALL 
+SELECT 4, "George", 8;
