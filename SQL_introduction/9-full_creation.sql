b9a6b0226308299a65d9c3761c18c1abb17ff66c
Ethan Benyayer <benyayerethan@gmail.com>
2023-03-13 22:00:33 +0100

    adding task 9

--- SQL_introduction/9-full_creation.sql
+++ SQL_introduction/9-full_creation.sql
@@ -1,3 +1,4 @@
+-- Write a script that creates a table second_table in the database hbtn_0c_0 in your MySQL server and add multiples rows.
 CREATE TABLE second_table(id int, name VARCHAR(256), score int);
 INSERT INTO second_table (id, name, score)  
 SELECT 1, "John", 10 
