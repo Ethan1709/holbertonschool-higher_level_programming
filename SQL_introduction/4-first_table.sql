e9cbcfae535f77bce6dbad773756fae8dc74580a
Ethan Benyayer <benyayerethan@gmail.com>
2023-03-13 12:11:31 +0100

    adding task 4

--- SQL_introduction/4-first_table.sql
+++ SQL_introduction/4-first_table.sql
@@ -1,6 +1,6 @@
 -- Write a script that creates a table called first_table in the current database
 USE hbtn_0c_0;
-CREATE TABLE IF EXISTS first_table (
+CREATE TABLE IF NOT EXISTS first_table (
     id INT,
     name VARCHAR(256)
 );
