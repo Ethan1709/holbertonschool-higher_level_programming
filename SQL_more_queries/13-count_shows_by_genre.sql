0228ade7513a6bdc6b2b55326e23c1cff233571f
Ethan Benyayer <benyayerethan@gmail.com>
2023-03-14 23:52:07 +0100

    adding task 13

---
+++ SQL_more_queries/13-count_shows_by_genre.sql
@@ -0,0 +1,5 @@
+-- Write a script that lists all genres from hbtn_0d_tvshows and displays the number of shows linked to each.
+SELECT genre, COUNT(*) AS number_of_shows
+FROM tv_genres
+LEFT JOIN tv_show_genres ON tv_genres.title = tv_show_genres.show_id
+ORDER BY number_of_shows DESC;
