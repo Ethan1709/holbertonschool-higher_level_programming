fb84c8c8fda708b99c0b48973066a9a29a1bdeb6
Ethan Benyayer <benyayerethan@gmail.com>
2023-03-14 21:33:32 +0100

    adding task 10

--- SQL_more_queries/10-genre_id_by_show.sql
+++ SQL_more_queries/10-genre_id_by_show.sql
@@ -1,5 +1,5 @@
 -- Write a script that lists all shows contained in hbtn_0d_tvshows that have at least one genre linked.
 SELECT tv_shows.title, tv_show_genres.genre_id 
-FROM tv_shows, tv_show_genres
-WHERE tv_show_genres.genre_id = tv_shows.title
+FROM tv_shows, tv_show_genres, tv_genres
+WHERE tv_genres.id = tv_shows.title
 ORDER BY tv_shows.title ASC, tv_show_genres.genre_id;
