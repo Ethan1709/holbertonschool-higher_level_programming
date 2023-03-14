6e6a738245c86c8bd771f695dee45c2eb4500c92
Ethan Benyayer <benyayerethan@gmail.com>
2023-03-15 00:07:44 +0100

    adding task 13

--- SQL_more_queries/13-count_shows_by_genre.sql
+++ SQL_more_queries/13-count_shows_by_genre.sql
@@ -2,5 +2,5 @@
 SELECT tv_genres.name AS genre, COUNT(*) AS number_of_shows
 FROM tv_genres
 JOIN tv_show_genres ON tv_genres.name = tv_show_genres.show_id
-GROUP BY tv_genres.name 
+GROUP BY tv_genres.id
 ORDER BY number_of_shows DESC;
