153116b010b3022114a6b1b76a4d2045f3335ee6
Ethan1709 <benyayerethan@gmail.com>
2023-03-15 22:53:06 +0100

    adding task 10

---
+++ python-object_relational_mapping/10-model_state_my_get.py
@@ -0,0 +1,21 @@
+#!/usr/bin/python3
+"""States class"""
+
+from model_state import Base, State
+import sys
+from sqlalchemy import create_engine, text
+from sqlalchemy.orm import sessionmaker
+
+
+if __name__ == "__main__":
+    username = sys.argv[1]
+    password = sys.argv[2]
+    database = sys.argv[3]
+    state_name_searched = sys.argv[4]
+
+    engine = create_engine("mysql+mysqldb://{}:{}@localhost:3306/{}".
+                           format(username, password, database))
+
+    r = engine.execute(text("SELECT states.id FROM states WHERE name = state_name_searched;"))
+    for row in r.fetchall():
+        print("{}".format(row[0]))
