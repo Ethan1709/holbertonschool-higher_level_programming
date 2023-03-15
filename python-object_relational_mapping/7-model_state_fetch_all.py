5cd77cb4904e50c469fa7208647935d95a6d79c6
Ethan1709 <benyayerethan@gmail.com>
2023-03-15 21:50:38 +0100

    adding task 7

--- python-object_relational_mapping/7-model_state_fetch_all.py
+++ python-object_relational_mapping/7-model_state_fetch_all.py
@@ -2,15 +2,16 @@
 """States class"""
 
 from model_state import Base, State
-from sqlalchemy.orm import sessionmaker
-from sqlalchemy import create_engine, text
 import sys
+from sqlalchemy import create_engine, text
+from sqlalchemy.orm import sessionmaker
+
 
 if __name__ == "__main__":
     username = sys.argv[1]
     password = sys.argv[2]
     database = sys.argv[3]
-    
+
     engine = create_engine("mysql+mysqldb://{}:{}@localhost:3306/{}".
                            format(username, password, database))
 
