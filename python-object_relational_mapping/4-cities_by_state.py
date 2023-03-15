3e79bae0dee15f405e0d738a46a3b27752bbcaca
Ethan1709 <benyayerethan@gmail.com>
2023-03-15 19:36:19 +0100

    adding task 4

---
+++ python-object_relational_mapping/4-cities_by_state.py
@@ -0,0 +1,26 @@
+#!/usr/bin/python3
+"""lists all cities from the database hbtn_0e_0_usa"""
+
+import MySQLdb
+import sys
+
+if __name__ == "__main__":
+    username = sys.argv[1]
+    password = sys.argv[2]
+    database = sys.argv[3]
+    connection = MySQLdb.connect(user=username, passwd=password,
+                                 db=database, host="localhost",
+                                 port=3306)
+
+    cursor = connection.cursor()
+    request = "SELECT cities.id, cities.name, states.name\
+        FROM cities, states ORDER BY cities.id"
+    cursor.execute(request)
+
+    cityList = cursor.fetchall()
+
+    for city in cityList:
+        print(city)
+
+    cursor.close()
+    connection.close()
