e460db24c9de582f15f908f3894b268ace0e948d
Ethan1709 <benyayerethan@gmail.com>
2023-03-15 20:56:36 +0100

    adding task 6

--- python-object_relational_mapping/model_state.py
+++ python-object_relational_mapping/model_state.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python3
-"""class State"""
+"""class States"""
 
 from sqlalchemy import Column, Integer, String
 from sqlalchemy.ext.declarative import declarative_base
@@ -7,6 +7,7 @@ from sqlalchemy.ext.declarative import declarative_base
 Base = declarative_base
 
 class States(Base):
+    """class States"""
     __tablename__ = 'states'
     id = Column(Integer, primary_key=True, autoincrement=True)
     name = Column(String)
