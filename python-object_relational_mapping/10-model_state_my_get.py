08971386aa3311265cef98fe68c79e123b20c505
Ethan1709 <benyayerethan@gmail.com>
2023-03-15 23:42:56 +0100

    adding task 10

--- python-object_relational_mapping/10-model_state_my_get.py
+++ python-object_relational_mapping/10-model_state_my_get.py
@@ -17,7 +17,7 @@ if __name__ == "__main__":
                            format(username, password, database))
 
     r = engine.execute("SELECT states.id FROM states WHERE name = %s", (state_searched,))
-    if r.rowcount > 1:
+    if r.rowcount > 0:
         for row in r:
             print(row[0])
     else:
