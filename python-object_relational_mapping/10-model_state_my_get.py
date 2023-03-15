2cf9aeb6e63bacca0c029e01537585e738ceea49
Ethan1709 <benyayerethan@gmail.com>
2023-03-15 23:19:02 +0100

    adding task 10

--- python-object_relational_mapping/10-model_state_my_get.py
+++ python-object_relational_mapping/10-model_state_my_get.py
@@ -16,7 +16,7 @@ if __name__ == "__main__":
     engine = create_engine("mysql+mysqldb://{}:{}@localhost:3306/{}".
                            format(username, password, database))
 
-    r = engine.execute
-    (text("SELECT states.id FROM states WHERE name = :state_name_searched;"))
+    query = text("SELECT states.id FROM states WHERE name S")
+    r = engine.execute(query, (state_name_searched,))
     for row in r.fetchall():
         print(row[0])
