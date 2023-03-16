d75c1fe4a7107990ce74cdc6f93bbe8b5bdd8790
Ethan1709 <benyayerethan@gmail.com>
2023-03-16 01:10:05 +0100

    adding task 12

---
+++ python-object_relational_mapping/12-model_state_update_id_2.py
@@ -0,0 +1,19 @@
+#!/usr/bin/python3
+"""States class"""
+
+from model_state import Base, State
+import sys
+from sqlalchemy import create_engine, text
+from sqlalchemy.orm import sessionmaker
+
+
+if __name__ == "__main__":
+    username = sys.argv[1]
+    password = sys.argv[2]
+    database = sys.argv[3]
+
+    engine = create_engine("mysql+mysqldb://{}:{}@localhost:3306/{}".
+                           format(username, password, database))
+
+    result = engine.execute(text("UPDATE states SET name = 'New Mexico' WHERE id = 2;"))
+    row = result.fetchone()
