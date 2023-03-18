783f869221682f5f4cb03626a3e6b6c8f2482f37
Ethan1709 <benyayerethan@gmail.com>
2023-03-18 21:11:11 +0100

    updating task 10 with session

--- python-object_relational_mapping/10-model_state_my_get.py
+++ python-object_relational_mapping/10-model_state_my_get.py
@@ -3,7 +3,7 @@
 
 from model_state import Base, State
 import sys
-from sqlalchemy import create_engine, text
+from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
 
 
@@ -11,16 +11,17 @@ if __name__ == "__main__":
     username = sys.argv[1]
     password = sys.argv[2]
     database = sys.argv[3]
-    state_searched = sys.argv[4]
+    name_searched = sys.argv[4]
 
     engine = create_engine("mysql+mysqldb://{}:{}@localhost:3306/{}".
                            format(username, password, database))
 
-    query = "SELECT states.id FROM states WHERE name = %s"
-    s = (state_searched,)
-    r = engine.execute(query, s)
-    if r.rowcount > 0:
-        for row in r:
-            print(row[0])
+    Base.metadata.create_all(engine)
+    Session = sessionmaker(bind=engine)
+    session = Session()
+    q = session.query(State).filter(State.name == name_searched).first()
+    if q:
+        print("{}".format(q.id))
     else:
         print("Not found")
+    session.close()
