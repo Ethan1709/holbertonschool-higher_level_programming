8713375f3887ce25161f362667951e81bb970274
Ethan1709 <benyayerethan@gmail.com>
2023-03-18 20:28:12 +0100

    updating task with session

--- python-object_relational_mapping/7-model_state_fetch_all.py
+++ python-object_relational_mapping/7-model_state_fetch_all.py
@@ -3,7 +3,7 @@
 
 from model_state import Base, State
 import sys
-from sqlalchemy import create_engine, text
+from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
 
 
@@ -15,6 +15,9 @@ if __name__ == "__main__":
     engine = create_engine("mysql+mysqldb://{}:{}@localhost:3306/{}".
                            format(username, password, database))
 
-    result = engine.execute(text("SELECT * FROM states;"))
-    for row in result.fetchall():
-        print("{}: {}".format(row[0], row[1]))
+    Base.metadata.create_all(engine)
+    Session = sessionmaker(bind=engine)
+    session = Session()
+    for state in session.query(State).order_by(State.id).all():
+        print("{}: {}".format(state.id, state.name))
+    session.close()
