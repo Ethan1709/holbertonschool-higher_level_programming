d422b97680e01ba0b487285310ab6188b44ff83e
Ethan1709 <benyayerethan@gmail.com>
2023-03-18 20:45:15 +0100

    updating task 8 with session

--- python-object_relational_mapping/8-model_state_fetch_first.py
+++ python-object_relational_mapping/8-model_state_fetch_first.py
@@ -18,6 +18,6 @@ if __name__ == "__main__":
     Base.metadata.create_all(engine)
     Session = sessionmaker(bind=engine)
     session = Session()
-    for state in session.query(State).filter(State.id == 1).all():
-        print("{}: {}".format(state.id, state.name))
+    q = session.query(State).filter(State.id == 1).one()
+    print("{}: {}".format(q.id, q.name))
     session.close()
