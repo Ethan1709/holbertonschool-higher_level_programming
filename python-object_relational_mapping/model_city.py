76314c9e47ca9edb67a2a766c239eafe5104100b
Ethan1709 <benyayerethan@gmail.com>
2023-03-18 22:10:37 +0100

    adding files for task 14

---
+++ python-object_relational_mapping/model_city.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+"""City class"""
+
+from sqlalchemy import Column, String, Integer, CHAR, ForeignKey
+from sqlalchemy.ext.declarative import declarative_base
+
+Base = declarative_base()
+
+
+class City(Base):
+    """City class"""
+    __tablename__ = "cities"
+    id = Column(Integer, primary_key=True, nullable=False,
+                unique=True, autoincrement=True)
+    name = Column(String(128), nullable=False)
+    state_id = Column(Integer, ForeignKey('states.id'), nullable=False)
