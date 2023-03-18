0fa6a76cfed014fa4abdecdb018f6e47d0b7c350
Ethan1709 <benyayerethan@gmail.com>
2023-03-18 20:41:32 +0100

    updating task 8 with session

--- python-object_relational_mapping/8-model_state_fetch_first.py
+++ python-object_relational_mapping/8-model_state_fetch_first.py
@@ -3,7 +3,7 @@
 
 from model_state import Base, State
 import sys
-from sqlalchemy import create_engine, text
+from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
 
 
@@ -15,9 +15,9 @@ if __name__ == "__main__":
     engine = create_engine("mysql+mysqldb://{}:{}@localhost:3306/{}".
                            format(username, password, database))
 
-    result = engine.execute(text("SELECT * FROM states;"))
-    row = result.fetchone()
-    if row is not None:
-        print("{}: {}".format(row[0], row[1]))
-    else:
-        print("Nothing")
+    Base.metadata.create_all(engine)
+    Session = sessionmaker(bind=engine)
+    session = Session()
+    for state in session.query(State).filter(State.id == 1).all():
+        print("{}: {}".format(state.id, state.name))
+    session.close()
