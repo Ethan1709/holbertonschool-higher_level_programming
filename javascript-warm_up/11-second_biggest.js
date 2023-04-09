3e6c5e90d33fb7078ffb510ba0b914d0f013ce41
Ethan1709 <benyayerethan@gmail.com>
2023-04-09 14:36:56 +0200

    adding task 11

--- javascript-warm_up/11-second_biggest.js
+++ javascript-warm_up/11-second_biggest.js
@@ -5,14 +5,12 @@ const args_len = args.length;
 if (args_len === 0 || args_len === 1) {
   console.log('0');
 } else {
-  console.log(args);
   for (let i = 0; i < args_len - 1; i++) {
     for (let j = 0; j < args_len - i - 1; j ++) {
         if (args[j] > args[j + 1]) {
             let x = args[j];
             args[j] = args[j + 1];
             args[j + 1] = x;
-            console.log(args);
         }
     }
 }
