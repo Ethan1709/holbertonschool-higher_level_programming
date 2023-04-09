4ade4b5fcc4d8891117231977f720da143c9926b
Ethan1709 <benyayerethan@gmail.com>
2023-04-09 10:30:36 +0200

    adding task 7

---
+++ javascript-warm_up/7-multi_c.js
@@ -0,0 +1,11 @@
+#!/usr/bin/node
+
+x  = process.argv[2];
+if (parseInt(x, 10)) {
+  for (let i = 0; i < x; i++) {
+    console.log('C is fun');
+  }
+} else {
+    console.log('Missing number of occurences');
+}
+
