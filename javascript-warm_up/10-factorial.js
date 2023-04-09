a1f813e6e7711fcae12c0418a0961bccec5f144c
Ethan1709 <benyayerethan@gmail.com>
2023-04-09 12:18:58 +0200

    adding task 10

---
+++ javascript-warm_up/10-factorial.js
@@ -0,0 +1,16 @@
+#!/usr/bin/node
+
+function fact (x) {
+    if (x == 0 ) {
+        return 1;
+    } else {
+        return x * fact(x - 1);
+    }
+}
+number = parseInt(process.argv[2]);
+if (number >= 0) {
+    const r = fact(number);
+    console.log(r);
+} else {
+    console.log('1')
+}
