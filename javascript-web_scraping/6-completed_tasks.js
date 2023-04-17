f6c14798b72c004abf1a217af4cb91a7532a831c
Ethan1709 <benyayerethan@gmail.com>
2023-04-17 14:34:51 +0200

    adding task 6

---
+++ javascript-web_scraping/6-completed_tasks.js
@@ -0,0 +1,37 @@
+#!/usr/bin/node
+
+const request = require('request');
+let c = 1;
+const dict = {};
+let k = 0;
+let t = 0;
+
+request({
+  url: 'https://jsonplaceholder.typicode.com/todos',
+  json: true
+}, (err, response, body) => {
+  if (err) {
+    console.log('Error:', err);
+  } for (const i in body) {
+    k += 1;
+    if (k % 20 === 0) {
+      if (body[i].completed === true) {
+        t += 1;
+      }
+      dict[c] = t;
+      t = 0;
+      c += 1;
+    }
+    if (body[i].userId === c) {
+      if (body[i].completed === true) {
+        t += 1;
+      }
+    }
+    // console.log(body[i].completed)
+  }
+  console.log(dict);
+  // console.log(body);
+});
+
+// body[i].userId = avoir l'userId
+// je dois filtrer les true dans body[i[j]]
