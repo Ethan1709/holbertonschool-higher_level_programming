96b0f84a085e938047e3978dc2072ec15cfe79c8
Ethan1709 <benyayerethan@gmail.com>
2023-04-17 10:14:54 +0200

    addding task 5

--- javascript-web_scraping/5-request_store.js
+++ javascript-web_scraping/5-request_store.js
@@ -2,10 +2,10 @@
 
 const request = require('request');
 const fs = require('fs');
-const api_url = String(process.argv[2]);
+const apiurl = String(process.argv[2]);
 const file = String(process.argv[3]);
 
-request.get(api_url, (error, response, body) => {
+request.get(apiurl, (error, response, body) => {
   if (error) {
     console.error(error);
     return;
