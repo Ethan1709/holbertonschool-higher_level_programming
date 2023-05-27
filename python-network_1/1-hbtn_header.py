be99c7d62163ff4229473f4163af1f5b5ac26efa
Ethan1709 <benyayerethan@gmail.com>
2023-05-28 01:45:14 +0200

    correcting for loop space

--- python-network_1/1-hbtn_header.py
+++ python-network_1/1-hbtn_header.py
@@ -4,6 +4,6 @@ from sys import argv
 
 with urlopen(argv[1]) as response:
     headers = response.getheaders()
-for i  in headers:
+for i in headers:
     if i[0] == 'X-Request-Id':
         print(i[1])
