8149576ef379e34b93fc7fe654e119378d651cf6
Ethan1709 <benyayerethan@gmail.com>
2023-05-28 00:52:49 +0200

    adding task 1

---
+++ python-network_1/1-hbtn_header.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+from urllib.request import urlopen
+from sys import argv
+
+with urlopen(str(argv[1])) as response:
+    headers = response.getheaders()
+for i  in headers:
+    if i[0] == 'X-Request-Id':
+        print(i[1])
