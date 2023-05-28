f089b108054146a16eb9a983da42b7c53e3ed863
Ethan1709 <benyayerethan@gmail.com>
2023-05-28 02:05:03 +0200

    adding task 4

---
+++ python-network_1/4-hbtn_status.py
@@ -0,0 +1,7 @@
+#!/usr/bin/python3
+import requests
+
+r = requests.get('https://intranet.hbtn.io/status')
+print('Body response:')
+print('\t- ' + 'type: ' + str(type(r.text)))
+print('\t- ' + 'content: ' + str(r.text))
