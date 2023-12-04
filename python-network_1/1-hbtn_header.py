53e56532892820f870f7ef4e6c0502c698290306
Ethan1709 <benyayerethan@gmail.com>
2023-12-05 00:59:25 +0100

    add request id

--- python-network_1/1-hbtn_header.py
+++ python-network_1/1-hbtn_header.py
@@ -3,7 +3,5 @@ from urllib.request import urlopen
 from sys import argv
 
 with urlopen(argv[1]) as response:
-    headers = response.getheaders()
-for i in headers:
-    if i[0] == 'X-Request-Id':
-        print(i[1])
+    x_request_id = response.headers.get('X-Request-Id')
+print (x_request_id)
