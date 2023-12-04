06be990715a72de3c5c47378968e459929c06fcc
Ethan1709 <benyayerethan@gmail.com>
2023-12-04 23:31:22 +0100

    fix http status

--- python-network_0/1-body.sh
+++ python-network_0/1-body.sh
@@ -1,3 +1,11 @@
 #!/bin/bash
 #Bash script that takes in a URL, sends a GET request to the URL, and displays the body of the response
-curl -s "$1"
+url="$1"
+body=$(curl -s -w "%{http_code}" "$url")
+http_status="${body: -3}"
+
+if  [ "$http_status" == 200 ]; then
+    echo "$body"
+else
+    echo ""
+fi
