8779b6ecc9c8f33fdcb5a9413f0c599805cddc6c
Ethan1709 <benyayerethan@gmail.com>
2023-12-05 13:08:41 +0100

    add comms and if main

--- python-network_1/8-json_api.py
+++ python-network_1/8-json_api.py
@@ -1,14 +1,19 @@
-#!/usr/bin/python3
+#!/usr/bin/env python3
+"""Comments"""
+
 import requests
 from sys import argv
 
-data = {'q': argv[1]}
+if __name__ == "__main__":
+    if argv[1] is None:
+       argv[1] = ""
+    data = {'q': argv[1]}
 
-try:
-    r = requests.post('https://chat.openai.com/', data=data)
-    j = r.json()
-    json_id = j['id']
-    json_name = j['name']
-    print(f'[{json_id}], {json_name}')
-except ValueError:
-    print('Not a valid JSON')
+    try:
+        r = requests.post('http://0.0.0.0:5000/search_user', data)
+        json_response = r.json()
+        json_id = json_response['id']
+        json_name = json_response['name']
+        print(f'[{json_id}], {json_name}')
+    except ValueError:
+        print('Not a valid JSON')
