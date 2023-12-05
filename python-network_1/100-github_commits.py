ef0d0bccbc2e3563a866c8d457af4747caac5c2c
Ethan1709 <benyayerethan@gmail.com>
2023-12-05 14:03:04 +0100

    change except

--- python-network_1/100-github_commits.py
+++ python-network_1/100-github_commits.py
@@ -16,5 +16,7 @@ if __name__ == "__main__":
             commit_sha = repo_data[i].get("sha")
             author = repo_data[i].get("commit").get("author").get("name")
             print(f'{commit_sha}: {author}')
-    except:
+    except IndexError:
+        pass
+    except KeyError:
         pass
