01ccc044665b05f93eea01b120ba1a7b8132cfe2
Ethan1709 <benyayerethan@gmail.com>
2023-12-05 13:17:51 +0100

    add comms and if main

--- python-network_1/10-my_github.py
+++ python-network_1/10-my_github.py
@@ -1,14 +1,17 @@
 #!/usr/bin/python3
+"""Comments"""
+
 import requests
 from sys import argv, exit
 
-username = argv[1]
-token = argv[2]
+if __name__ == "__main__":
+    username = argv[1]
+    token = argv[2]
 
-try:
-    r = requests.get('https://api.github.com/users/Ethan1709', auth=(username, token))
-    user_data = r.json()
-    user_id = user_data['id']
-    print(user_id)
-except requests.exceptions.RequestException:
-    exit(1)
+    try:
+        r = requests.get('https://api.github.com/users/Ethan1709', auth=(username, token))
+        user_data = r.json()
+        user_id = user_data['id']
+        print(user_id)
+    except requests.exceptions.RequestException:
+        exit(1)
