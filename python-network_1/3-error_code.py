46cf93e544547349926353bd7179c5ffec2fe2d0
Ethan1709 <benyayerethan@gmail.com>
2023-12-05 01:36:13 +0100

    fix comm

--- python-network_1/3-error_code.py
+++ python-network_1/3-error_code.py
@@ -1,13 +1,16 @@
 #!/usr/bin/python3
+"""Python script that takes in a URL"""
+
 from urllib.request import urlopen
 from urllib.error import HTTPError
 from sys import argv
 
-url = argv[1]
+if __name__ == "__main__":
+    url = argv[1]
 
-try:
-    with urlopen(url) as response:
-        r = response.read().decode('utf-8')
-    print(r)
-except HTTPError as e:
-    print('Error code: ' + str(e.code))
+    try:
+        with urlopen(url) as response:
+            r = response.read().decode('utf-8')
+        print(r)
+    except HTTPError as e:
+        print('Error code: ' + str(e.code))
