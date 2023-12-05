602dff37b16fb77456f767927dc710e089bdd801
Ethan1709 <benyayerethan@gmail.com>
2023-12-05 13:14:07 +0100

    add no result except

--- python-network_1/8-json_api.py
+++ python-network_1/8-json_api.py
@@ -5,11 +5,8 @@ import requests
 from sys import argv
 
 if __name__ == "__main__":
-    if argv[1] is None:
-        argv[1] = ""
-    data = {'q': argv[1]}
-
     try:
+        data = {'q': argv[1]}
         r = requests.post('http://0.0.0.0:5000/search_user', data)
         json_response = r.json()
         json_id = json_response['id']
@@ -17,3 +14,5 @@ if __name__ == "__main__":
         print(f'[{json_id}] {json_name}')
     except ValueError:
         print('Not a valid JSON')
+    except IndexError:
+        print('No result')
