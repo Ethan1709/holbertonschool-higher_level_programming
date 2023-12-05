5942db1f5be90638fa8c722f16390579799a4b44
Ethan1709 <benyayerethan@gmail.com>
2023-12-05 12:58:21 +0100

    add comms and if main

--- python-network_1/7-error_code.py
+++ python-network_1/7-error_code.py
@@ -1,14 +1,17 @@
 #!/usr/bin/python3
+"""Comments"""
+
 import requests
 from sys import argv, exit
 
-url = argv[1]
+if __name__ == "__main__":
+    url = argv[1]
 
-try:
-    r = requests.get(url)
-    if r.status_code == 200 or r.status_code < 400:
-        print(r.text)
-    else:
-        print('Error code:', r.status_code)
-except requests.exceptions.RequestException as e:
-    exit(1)
+    try:
+        r = requests.get(url)
+        if r.status_code == 200 or r.status_code < 400:
+            print(r.text)
+        else:
+            print('Error code:', r.status_code)
+    except requests.exceptions.RequestException as e:
+        exit(1)
