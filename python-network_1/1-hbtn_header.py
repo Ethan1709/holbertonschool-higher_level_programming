cf2e63d9f1bb413047d98f0ae18844c43bd6ff73
Ethan1709 <benyayerethan@gmail.com>
2023-12-05 01:04:39 +0100

    add comm

--- python-network_1/1-hbtn_header.py
+++ python-network_1/1-hbtn_header.py
@@ -5,5 +5,6 @@ from urllib.request import urlopen
 from sys import argv
 
 with urlopen(argv[1]) as response:
+    """ Comments """
     x_request_id = response.headers.get('X-Request-Id')
 print (x_request_id)
