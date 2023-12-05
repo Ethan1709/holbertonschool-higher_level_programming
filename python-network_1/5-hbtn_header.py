47d1445bbed284899047a2e313234fe463cb0956
Ethan1709 <benyayerethan@gmail.com>
2023-12-05 12:52:32 +0100

    add if main

--- python-network_1/5-hbtn_header.py
+++ python-network_1/5-hbtn_header.py
@@ -4,7 +4,8 @@
 import requests
 from sys import argv
 
-url = argv[1]
+if __name__ == "__main__":
+    url = argv[1]
 
-r = requests.get(url)
-print(r.headers.get('X-Request-Id'))
+    r = requests.get(url)
+    print(r.headers.get('X-Request-Id'))
