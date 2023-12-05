dcd33067e505611377718ac10c65d4f0c3bc5a8a
Ethan1709 <benyayerethan@gmail.com>
2023-12-05 01:15:30 +0100

    fix pycode

--- python-network_1/2-post_email.py
+++ python-network_1/2-post_email.py
@@ -1,12 +1,14 @@
 #!/usr/bin/python3
+"""Python script that takes in a URL"""
+
 from urllib.request import urlopen
 from sys import argv
 
+if __name__ == "__main__":
+    url = argv[1]
+    email = argv[2]
+    data = {'email': email}
 
-email = argv[2]
-url = argv[1]
-data = {'email': email}
-
-with urlopen(url) as response:
-    r = response.read().decode('utf-8')
-print('Your email is: ' + r)
+    with urlopen(url) as response:
+        r = response.read().decode('utf-8')
+    print('Your email is: ' + r)
