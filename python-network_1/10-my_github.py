fd1c291b16d48f7c27dc6450d6835815aff4ab69
Ethan1709 <benyayerethan@gmail.com>
2023-12-05 13:33:14 +0100

    add comms and basichttpauth

--- python-network_1/10-my_github.py
+++ python-network_1/10-my_github.py
@@ -1,17 +1,11 @@
-#!/usr/bin/python3
+#!/usr/bin/env python3
 """Comments"""
 
 import requests
 from sys import argv, exit
+from requests.auth import HTTPBasicAuth
 
 if __name__ == "__main__":
-    username = argv[1]
-    token = argv[2]
-
-    try:
-        r = requests.get('https://api.github.com/users/Ethan1709', auth=(username, token))
-        user_data = r.json()
-        user_id = user_data['id']
-        print(user_id)
-    except requests.exceptions.RequestException:
-        exit(1)
+    auth = HTTPBasicAuth(argv[1], argv[2])
+    r = requests.get("https://api.github.com/user", auth=auth)
+    print(r.json().get("id"))
