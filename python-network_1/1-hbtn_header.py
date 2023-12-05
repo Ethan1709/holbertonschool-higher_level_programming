7299e7fdcb5b13f6d3a34225e1fbca7fe9dd47e2
Ethan1709 <benyayerethan@gmail.com>
2023-12-05 01:07:49 +0100

    add comm

--- python-network_1/1-hbtn_header.py
+++ python-network_1/1-hbtn_header.py
@@ -1,12 +1,11 @@
 #!/usr/bin/python3
 """Displays the X-Request-Id header variable of a request to a given URL.
-
-Usage: ./1-hbtn_header.py <URL>
 """
 
-from urllib.request import urlopen
-from sys import argv
+if __name__ == "__main__":
+    from urllib.request import urlopen
+    from sys import argv
 
-with urlopen(argv[1]) as response:
-    x_request_id = response.headers.get('X-Request-Id')
-print (x_request_id)
+    with urlopen(argv[1]) as response:
+        x_request_id = response.headers.get('X-Request-Id')
+    print (x_request_id)
