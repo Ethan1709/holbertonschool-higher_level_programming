e4a041eba31fefdc5928af52452c0224bccfd096
Ethan1709 <benyayerethan@gmail.com>
2023-12-05 16:07:50 +0100

    change param1 pos

--- python-network_0/101-post_json.sh
+++ python-network_0/101-post_json.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 #Bash script that sends a JSON POST request to a URL passed as the first argument, and displays the body of the response.
-curl -s -X POST -d "@$2" "$1"
+curl -s -L -X "$1" POST -d "@$2"
